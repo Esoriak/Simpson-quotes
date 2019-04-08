b532ae7474c31219645c8e577b19ff063cb3baa5
Esoriak <somahaut.dev@gmail.com>
2019-04-08 18:05:19 +0200

    jesuispolie

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg'
 import Quote from './Components/Quote'
+import Lamp from './Components/Lamp'
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        on: false
+      };
+  } 
+  handleClick = () => {
+    this.setState({on: !this.state.on});
+  };
   render() {
-    return (
+      const atWork = this.state.on ?'Homer in working' : 'Homer is NOOOOT working';
+      const spinning = this.state.on ? ' faster' : 'App-logo';
+      return (
       <div className="App">
         <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
+          <button onClick={this.handleClick}
+          className={atWork}>{atWork} </button>
         </header>
+        <Lamp on/>
+        <Lamp />
         <Quote
           quote="I believe the children are the future... Unless we stop them now!"
           character="Homer Simpson"
